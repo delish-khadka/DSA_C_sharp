75d63abe3082dfd0b1cae5a414c9ed9d70274861
delish-khadka <delishkhadka@gmail.com>
2024-08-31 21:36:54 +0545

    some recursion before getting started

---
+++ RecursionExercises/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Top-level statements
+var calculator = new FactorialCalculator();
+int result = calculator.Factorial(5);
+Console.WriteLine("Factorial is: " + result);
+
+class FactorialCalculator
+{
+    public int Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * Factorial(n - 1);
+    }
+}
